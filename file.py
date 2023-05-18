78dce2c61491b816aced873b1d5b87fc9da226fc
MaryBark <97518012+MaryBark@users.noreply.github.com>
2023-05-18 13:15:10 +0300

    Update file.py

--- file.py
+++ file.py
@@ -44,3 +44,57 @@ while i < n - 2:
     i = i + 1
 
 print("Число Фибоначи", fibNumbers2)
+
+
+
+
+
+# поиск по левому неведомому массиву
+print("Бинарный поиск")
+for i in array:
+    if i == 7:
+        print("Ура нашли!!!")
+
+# бинарный поиск по левому неведомому массиву по рекурсии с помощью функции
+num = 4
+# def Bynary_poisk(arr, num):
+first = 0
+last = len(array)-1
+index = -1
+while (first <= last) and (index == -1):
+    mid = (first+last)//2
+    if array[mid] == num:
+        index = mid
+    else:
+         if num < array[mid]:
+            last = mid -1
+         else:
+            first = mid +1
+
+print(index)
+    # return index
+
+
+
+
+# f ntgthm aeyrwbz!!!!!
+def Bynary_poisk(arr, num):
+    first = 0
+    last = len(array)-1
+    index = -1
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if array[mid] == num:
+            index = mid
+        else:
+            if num < array[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
+
+>>> Bynary_poisk([2, 2, 3, 6, 7, 7], num)
+
+
+
+# >>> print(Bynary_poisk([10,20,30,40,50], 20))
